42a3c6f6216d70d704c03be1766df6fb3be2ae5b
trebortc <trebortc@hotmail.com>
2019-07-01 21:19:13 -0500

    Agregar movimiento a flota de aliens

--- game_functions.py
+++ game_functions.py
@@ -96,3 +96,24 @@ def get_number_rows(ai_settings, ship_height, alien_heigt):
     number_rows = int(available_space_y/(2 * alien_heigt))
     return number_rows
 
+def update_aliens(ai_settings, aliens):
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
+
+def check_fleet_edges(ai_settings, aliens):
+    """Respond appropriately if any aliens have reached an edge """
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+def change_fleet_direction(ai_settings, aliens):
+    """Drop the entire fleet and change the fleet´s direcion """
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *=-1
+
+    
+
+
+
--- settings.py
+++ settings.py
@@ -6,10 +6,18 @@ class Settings():
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
 
+        #Ship
         self.ship_speed_factor = 1.5
 
+        #Bullet
         self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 255, 60)
         self.bullets_allowed = 3
+
+        #Alien
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        #fleet_direction of 1 represents right -1 represents left
+        self.fleet_direction = 1
