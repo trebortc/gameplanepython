a079f99369f9838087e180c0ac10756ff2a1233b
trebortc <trebortc@hotmail.com>
2019-07-07 23:36:56 -0500

    Agregado score, vidas, nivel por definir

--- InvasionAlien.py
+++ InvasionAlien.py
@@ -1,10 +1,14 @@
 import sys
 import pygame
 from settings import Settings
+from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
 from ship import Ship
 from alien import Alien
 import game_functions as gf
 from pygame.sprite import Group
+
 
 def run_game():
     pygame.init()
@@ -14,20 +18,25 @@ def run_game():
 
     ship = Ship(ai_settings, screen)
     alien = Alien(ai_settings, screen)
-    
+    play_button = Button(ai_settings, screen, 'Play')
+
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+
     #Make a group to store bullets y aliens
     bullets = Group()
     aliens = Group()
 
     gf.create_fleet(ai_settings, screen, ship, aliens)
 
-
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_aliens(ai_settings, aliens)
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
+        if stats.game_active:
+                ship.update()
+                gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+                gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
+        
 run_game()
 
--- ship.py
+++ ship.py
@@ -1,7 +1,9 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship():
+class Ship(Sprite):
     def __init__(self, ai_settings, screen):
+        super(Ship, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
 
@@ -30,4 +32,9 @@ class Ship():
     
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Center the ship on the screen"""
+        self.center = self.screen_rect.centerx
+        
 
